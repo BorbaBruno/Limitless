2e68e261743c0096de59ca8b629816faae7feb24
BorbaBruno <brunodeborba@outlook.com>
2022-09-27 13:11:17 -0300

    sobre e imagens

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ function openMenu() {
     document.body.classList.add('menu-expanded')
   }
   
-  function closeMenu() {
+function closeMenu() {
     document.body.classList.remove('menu-expanded')
+}
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+  
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(products)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+      navegacao.classList.add('scroll')
+    } else {
+      navegacao.classList.remove('scroll')
+    }
   }
+
+window.addEventListener('scroll', onScroll)
+onScroll()
+
