fe75c669a283eb779dc5265548a42e4b5115b942
BorbaBruno <brunodeborba@outlook.com>
2022-10-26 16:10:02 -0300

    layout adjusts

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ window.addEventListener('load', start)
 
 function nextImage() {
 
-images[currentImageIndex].classList.remove('selected')
+    images[currentImageIndex].classList.remove('selected')
 
-currentImageIndex++
+    currentImageIndex++
 
-if(currentImageIndex >= max)
+    if(currentImageIndex >= max)
     currentImageIndex = 0
 
-images[currentImageIndex].classList.add('selected')
+    images[currentImageIndex].classList.add('selected')
 }
 
 function nextImage2() {
@@ -47,85 +47,85 @@ setInterval(() => {
 }
 
 function onScroll() {
-showNavOnScroll()
-showBackToTopButtonOnScroll()
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
 
-activateMenuAtCurrentSection(home)
-activateMenuAtCurrentSection(project)
-activateMenuAtCurrentSection(about)
-activateMenuAtCurrentSection(contact)
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(project)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
 
 function activateMenuAtCurrentSection(section) {
 
-const targetLine = scrollY + innerHeight / 2
+    const targetLine = scrollY + innerHeight / 2
 
 // Verificar se a seção passou da linha // 
 // Quais dados vou precisar? //
 
-const sectionTop = section.offsetTop
-const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
 
-const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndsAt = sectionTop + sectionHeight
 
-const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
-const sectionId = section.getAttribute("id")
-const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    const sectionId = section.getAttribute("id")
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
-menuElement.classList.remove("active")
-if(sectionBoundaries) {
-menuElement.classList.add("active")
-}
+    menuElement.classList.remove("active")
+    if(sectionBoundaries) {
+    menuElement.classList.add("active")
+    }
 }
 
 function showNavOnScroll() {
-if (scrollY > 0) {
-navegacao.classList.add('scroll')
-} else {
-navegacao.classList.remove('scroll')
-}
+    if (scrollY > 0) {
+    navegacao.classList.add('scroll')
+    } else {
+    navegacao.classList.remove('scroll')
+    }
 }
 
-function showBackToTopButtonOnScroll() {
-if (scrollY > 400) {
-backToTopButton.classList.add('show')
-} else {
-backToTopButton.classList.remove('show')
-}
+    function showBackToTopButtonOnScroll() {
+    if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+    } else {
+    backToTopButton.classList.remove('show')
+    }
 }
 
 function openMenu() {
-document.body.classList.add('menu-expanded')
+    document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-document.body.classList.remove('menu-expanded')
+    document.body.classList.remove('menu-expanded')
 }
 
 window.addEventListener('scroll', onScroll)
 onScroll()
 
 ScrollReveal({
-origin: 'top',
-distance: '30px',
-duration: 975
-}).reveal(`
-#home, 
-#home img, 
-#home 
-.stats, 
-#products,
-#products header,
-#products .card,
-#about,
-#about header,
-#about content,
-#contact`)
+    origin: 'top',
+    distance: '30px',
+    duration: 975
+    }).reveal(`
+    #home, 
+    #home img, 
+    #home 
+    .stats, 
+    #products,
+    #products header,
+    #products .card,
+    #about,
+    #about header,
+    #about content,
+    #contact`)
 
 
 
